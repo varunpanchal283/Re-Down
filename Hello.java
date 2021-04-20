d14f2df34d10d59bfb8cc6c2683e44604d937f83
varunpanchal283 <47024271+varunpanchal283@users.noreply.github.com>
2021-04-20 11:48:39 +0530

    Delete Hello.java

--- Hello.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-import java.io.*;
-public class Hello{
-	static String path = System.getProperty("user.dir");
-	public static void main(String args[]){
-		String pathid=path+"c:\\";
-		System.out.println(pathid);
-	}
-}
